4c40347763f342d86a557afcd6653b135a775b2a
marcoazzurrini <marco.azzurrini.art@gmail.com>
2020-03-24 22:32:40 +0100

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,33 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  plugins: [
+    `gatsby-plugin-emotion`,
+    `gatsby-plugin-advanced-sitemap`,
+    `gatsby-plugin-robots-txt`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Il Tetris piu' temuto del wild wild web.",
+        short_name: "Tetris",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
+        display: "standalone",
+        icon: "src/images/icon.png", // This path is relative to the root of the site.
+
+        crossOrigin: `use-credentials`,
+      },
+    },
+    `gatsby-plugin-offline`,
+  ],
 }
---
+++ src/Styles/theme.js
@@ -0,0 +1,19 @@
+export const theme = {
+  colors: {
+    playerB: "#2D78D0",
+    playerA: "#A62DD0",
+    grey100: "#D2D2D2",
+    grey200: "#8E8E8E",
+    grey300: "#9C9C9C",
+    grey400: "#565656",
+  },
+  fontSize: {
+    text100: "14px",
+    text200: "16px",
+    text300: "20px",
+    text400: "25px",
+    text400: "31px",
+    text400: "39px",
+    text500: "48px",
+  },
+}
